c99fa37ca0cd6220643d896a23c2e6cfaf795e20
Maxim Markelow <max.markelow@gmail.com>
2018-11-08 23:36:22 +0300

    Added AssemblyInfo and changelog

---
+++ src/Monitor/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FreeSpaceAlert")]
+[assembly: AssemblyDescription("Tool for detecting lack of disk space and alerting via Email")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Markeli")]
+[assembly: AssemblyProduct("FreeSpaceAlert")]
+[assembly: AssemblyCulture("en-EN")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c8beb2e-8e42-4842-a9ee-43751ad0ccdb")]
+
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
